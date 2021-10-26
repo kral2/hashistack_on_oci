357e0b14af66bd9873ddf08f1bc0136331e01d2d
Çetin ARDAL <kral2@users.noreply.github.com>
2021-10-26 10:42:10 +0200

    feat(start.sh): launch VSCode at execution

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 set -e
 
 # Terminal rapid start.
-# Only works on macOS with iTerm installed. Sorry, but not sorry ¯\_(ツ)_/¯
+# Only works on macOS with iTerm and VSCode installed. Sorry, but not sorry ¯\_(ツ)_/¯
 [ `uname -s` != "Darwin" ] && return
 
 # open additional terminal shells
-open -a iTerm "build" && tree -L 1
+open -a iTerm "build" && tree -L 1 && code .
 open -a iTerm "provision"
 open -a iTerm "run"
 open -a iTerm "run"
